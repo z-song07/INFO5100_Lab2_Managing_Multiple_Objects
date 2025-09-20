e60e4ab1a055d53995361797a154d8daf718164e
z-song07 <ziwei.song07@spsmail.cuny.edu>
2025-09-20 15:17:13 -0400

    Initial Commit
    Set up the project structure.

---
+++ Vital_Signs/src/model/VitalSigns.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author csong
+ */
+public class VitalSigns {
+    
+    // attributes
+    String date;
+    float temperature;
+    double bloodPressure;
+    int pulse;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(float temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getBloodPressure() {
+        return bloodPressure;
+    }
+
+    public void setBloodPressure(double bloodPressure) {
+        this.bloodPressure = bloodPressure;
+    }
+
+    public int getPulse() {
+        return pulse;
+    }
+
+    public void setPulse(int pulse) {
+        this.pulse = pulse;
+    }
+
+}
